50e0bc9067ade75c4bf7d66df9324371087bb9ba
Md Yusuf Ansari <mdyusuf1202@gmail.com>
2025-11-01 15:04:41 +0530

    Add solutions for conditionals and loops exercises

---
+++ 03__loops/09_solution.py
@@ -0,0 +1,10 @@
+items = ["apple", "banana", "oragne", "apple", "mango"]
+
+unique_item = set()
+
+for item in items:
+    if item in unique_item:
+        print("Duplicate ", item)
+        break
+    unique_item.add(item)
+    
